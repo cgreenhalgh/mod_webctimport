50c99eb3d8ad643cf52fce1bc2be6a3cb9e89d19
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2011-10-02 09:44:36 +0100

    added webctgrant table

--- db/upgrade.php
+++ db/upgrade.php
@@ -143,5 +143,33 @@ function xmldb_webctimport_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2011080302, 'webctimport');        
     }
     
+    if ($oldversion < 2011101001) {
+    
+    	// Define table webctgrant to be created
+    	$table = new xmldb_table('webctgrant');
+    
+    	// Adding fields to table webctgrant
+    	$table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+    	$table->add_field('userid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('title', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+    	$table->add_field('description', XMLDB_TYPE_TEXT, 'small', null, null, null, null);
+    	$table->add_field('path', XMLDB_TYPE_TEXT, 'small', null, XMLDB_NOTNULL, null, null);
+    	$table->add_field('granted', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('grantedby', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('webcttype', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+    	$table->add_field('filesize', XMLDB_TYPE_INTEGER, '12', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
+    
+    	// Adding keys to table webctgrant
+    	$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+    
+    	// Conditionally launch create table for webctgrant
+    	if (!$dbman->table_exists($table)) {
+    		$dbman->create_table($table);
+    	}
+    
+    	// webctimport savepoint reached
+    	upgrade_mod_savepoint(true, 2011101001, 'webctimport');
+    }
+    
     return true;
 }
